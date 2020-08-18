63e11ea74496eff6b60f2cc23e4904111c20bf53
weldone-dev <roma.kalita2015@gmail.com>
2020-08-18 17:39:34 +0300

    Add base structure

---
+++ src/app/app.component.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import s from "./app.module.less";
+class MyComponent extends Component {
+  render() {
+    return <div className={s.inso}>Hello World</div>;
+  }
+}
+export default MyComponent;
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./app/app.component";
+import "./style.less"
+ReactDOM.render(<App />, document.querySelector("#root"));
